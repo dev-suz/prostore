478aa8dce48bb9fa2a7564fb44ae4e89c0a9fe04
Suzanne <dev.nonlinear@gmail.com>
2025-07-27 13:50:21 +0900

    shadcn

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Homepage = () => {
+  return (
+    <div>
+    <Button>
+      Button
+    </Button>
+    </div>
+  );
+};
+
+export default Homepage;
